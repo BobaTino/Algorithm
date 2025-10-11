ace5ee385fb6c469ed7aa90e44700969aff0bbb5
BobaTino <Longba.le@calbaptist.edu>
2025-10-10 22:16:55 -0700

    OptimizeHeapSort

--- src/OptimizeHeapSort.java
+++ src/OptimizeHeapSort.java
@@ -37,6 +37,91 @@
 //     remove last element
 //     heapify(heap, length(heap), 0)
 //     return top
+import java.util.ArrayList;
+import java.util.List;
+
+class Player {
+
+    String name;
+    int score;
+
+    Player(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}
+
 public class OptimizeHeapSort {
 
+    // Build Max Heap using Bottom-Up Construction
+    public static void buildMaxHeap(List<Player> players) {
+        int n = players.size();
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(players, n, i);
+        }
+    }
+
+    // Heapify a subtree rooted at index i
+    public static void heapify(List<Player> players, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && players.get(left).score > players.get(largest).score) {
+            largest = left;
+        }
+
+        if (right < n && players.get(right).score > players.get(largest).score) {
+            largest = right;
+        }
+
+        if (largest != i) {
+            swap(players, i, largest);
+            heapify(players, n, largest);
+        }
+    }
+
+    // Swap two players
+    public static void swap(List<Player> players, int i, int j) {
+        Player temp = players.get(i);
+        players.set(i, players.get(j));
+        players.set(j, temp);
+    }
+
+    // Extract top score player
+    public static Player extractMax(List<Player> players) {
+        if (players.size() == 0) {
+            return null;
+        }
+
+        Player top = players.get(0);
+        players.set(0, players.get(players.size() - 1));
+        players.remove(players.size() - 1);
+        heapify(players, players.size(), 0);
+        return top;
+    }
+
+    public static void updateLeaderboard(List<Player> players) {
+        buildMaxHeap(players);
+
+        System.out.println("Top Players:");
+        int topCount = Math.min(10, players.size());
+
+        for (int i = 0; i < topCount; i++) {
+            Player top = extractMax(players);
+            System.out.println((i + 1) + ". " + top.name + " - " + top.score);
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Player> players = new ArrayList<>();
+        players.add(new Player("Alice", 95));
+        players.add(new Player("Bob", 88));
+        players.add(new Player("Chris", 120));
+        players.add(new Player("Diana", 110));
+        players.add(new Player("Ethan", 102));
+        players.add(new Player("Faye", 99));
+
+        updateLeaderboard(players);
+    }
 }
